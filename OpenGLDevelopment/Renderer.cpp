c398d6e723d049db87b4d4844281aaa6458d2fec
Alexander Miller <alexmiller12@yahoo.com>
2016-03-11 00:15:25 -0500

    Created and implemented supercalss REnderer

---
+++ OpenGLDevelopment/Renderer.cpp
@@ -0,0 +1,88 @@
+#include "Renderer.h"
+
+//---------------------------------------------------------CONSTRUCTORS/DESTRUCTORS:
+
+Renderer::Renderer()
+{}
+
+Renderer::~Renderer()
+{}
+
+//------------------------------------------------------------------------FUNCTIONS:
+
+void defaultErrorCallback( int error, const char* description )
+{
+	fputs( description, stderr );
+	_fgetchar();
+}
+
+// Default key input callback  
+void defaultKeyCallback( GLFWwindow* window, 
+						 int key, 
+						 int scancode, 
+						 int action, 
+						 int mods )
+{
+	if( key == GLFW_KEY_ESCAPE && action == GLFW_PRESS )
+	{
+		glfwSetWindowShouldClose( window, GL_TRUE );
+	}		
+}
+
+//--------------------------------------------------------------------------METHODS:
+
+// Binds to the window's gl context
+void Renderer::bind()
+{
+	// TODO if(window) else, error
+	glfwMakeContextCurrent( window );
+}
+
+void Renderer::closeWindow()
+{
+	//Close OpenGL window and terminate GLFW  
+	glfwDestroyWindow( window );
+
+	//Finalize and clean up GLFW  
+	glfwTerminate();
+}
+
+// Creates a GLFW window and initializes GL context. Returns true upon success
+bool Renderer::createWindow() 
+{
+	//Set the error callback  
+	glfwSetErrorCallback( defaultErrorCallback );
+	//Initialize GLFW  
+	if( ! glfwInit() )
+	{
+		return false;
+	}
+	//Create a window and create its OpenGL context  
+	window = glfwCreateWindow( DEFAULT_WINDOW_WIDTH, 
+							   DEFUALT_WINDOW_HEIGHT, 
+							   DEFAULT_WINDOW_NAME, 
+							   NULL, 
+							   NULL );
+	//If the window couldn't be created  
+	if( ! window )
+	{
+		glfwTerminate();
+		return false;
+	}
+	// Make the context of the specified window current on the calling thread   
+	glfwMakeContextCurrent( window );
+	//Sets the key callback  
+	glfwSetKeyCallback( window, defaultKeyCallback );
+	//Initialize GLEW  
+	GLenum glewInitStatus = glewInit();
+	// Return true if status is OK
+	return glewInitStatus == GLEW_OK;
+}
+
+void Renderer::unbind()
+{
+	glfwMakeContextCurrent( NULL );
+}
+
+//--------------------------------------------------------------------------HELPERS:
+
---
+++ OpenGLDevelopment/Renderer.h
@@ -0,0 +1,51 @@
+#ifndef MY_GL_RENDERER
+#define MY_GL_RENDERER
+
+#include "Dependencies\glew\glew.h"
+#include "Dependencies\glfw\glfw3.h"
+
+#include <stdio.h>  
+#include <stdlib.h>  
+
+#include <Windows.h>
+
+class Renderer
+{
+	
+//----------------------------------------------------------------------------ENUMS:
+
+//------------------------------------------------------------------------CONSTANTS:
+	
+//---------------------------------------------------------------------------FIELDS:
+	
+protected:
+	GLFWwindow* window;
+
+private:	
+	const int DEFAULT_WINDOW_WIDTH	= 640;
+	const int DEFUALT_WINDOW_HEIGHT = 480;
+	const char* DEFAULT_WINDOW_NAME = "OpenGL Window";
+
+//---------------------------------------------------------CONSTRUCTORS/DESTRUCTORS:
+	
+public:
+	Renderer();
+	virtual ~Renderer();
+	
+//--------------------------------------------------------------------------METHODS:
+
+public:
+	void bind();
+	void closeWindow();
+	bool createWindow();
+	void unbind();
+
+private:
+
+//--------------------------------------------------------------GETTERS AND SETTERS:
+
+public:
+	
+};
+
+#endif
